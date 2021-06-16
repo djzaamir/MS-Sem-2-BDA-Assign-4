f22f852d7a5d245f3cff3829dd5453979538455f
Aamir <djzaamir@gmail.com>
2021-06-16 21:34:25 +0500

    Most of the work is completed for task # 2

--- Clustering-Consumer-With-Jaccord-Similarity.py
+++ Clustering-Consumer-With-Jaccord-Similarity.py
@@ -5,9 +5,14 @@ from pymongo import MongoClient
 import math
 import Levenshtein
 
+def jaccard_similarity(list1, list2):
+    s1 = set(list1)
+    s2 = set(list2)
+    return float(len(s1.intersection(s2)) / len(s1.union(s2)))
+
 def main():
 
-    print(" Starting Clustering Consumer....")
+    print(" Starting Clustering Consumer With Jaccord Similarity....")
 
     topic_name = "pokec_user_data_stream"  
     k_consumer_a = KafkaConsumer(topic_name,
@@ -16,7 +21,6 @@ def main():
                                  value_deserializer=lambda x: json.loads(x.decode("utf-8")))
 
     m_client = MongoClient()
-
     ids = []
     streamed_data = []
     clusters = {"a" : [0, 5] ,"b" : [5, 10],"c" : [10, 15],"d" : [15, 20] , "e" : [20, math.inf]}
@@ -27,29 +31,41 @@ def main():
         col_a = streaming_data.value["I_like_movies"]
         col_b = streaming_data.value["I_like_watching_movie"]
 
+        sentence = col_a + " " + col_b
         
-        # Making sure not to include duplicate data in clusters
         if u_id not in ids:
             ids.append(u_id)
 
+
             min_edit_distance = math.inf
+            edit_distance_computed = False
             for s_d in streamed_data:
                 
-                dis_a = Levenshtein.distance(s_d["GenderMF"], col_a)
-                dis_b = Levenshtein.distance(s_d["SmokingStatus"], col_b)
+                # Jaccord Similarity
+                j_sim = jaccard_similarity(sentence.split(), s_d.split())
+                if j_sim < 0.5:
+                    continue
+                
 
-                if dis_a + dis_b < min_edit_distance:
-                    min_edit_distance = dis_a + dis_b
+                edit_distance_computed = True
+                # Edit Distance
+                dist =  Levenshtein.distance(sentence, u_id)
+                if dist < min_edit_distance:
+                    min_edit_distance = dist
 
+
+            if edit_distance_computed == False:
+                continue
 
             group = "f"
+            print(f"Min-Edit-Distance for current sentence with other sentence = {min_edit_distance}")
             for c in clusters:
                 if min_edit_distance >= clusters[c][0] and min_edit_distance < clusters[c][1]:
                     group = c 
                     break
             
-            m_client.BDA.edit_distance_sim.insert_one({"group" : group})
-            streamed_data.append({"GenderMF": col_a, "SmokingStatus": col_b})
+            m_client.BDA.edit_distance_sim_with_jaccord.insert_one({"group" : group})
+            streamed_data.append(sentence)
             
      
 
